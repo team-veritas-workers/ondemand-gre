f7746294464e421e097f67bc245a2e957826d6b0
Jimmy Huynh <jkchuynh829@gmail.com>
2017-01-18 20:12:12 -0800

    Created getVideoData method using jquery post request

--- src/components/containers/app.jsx
+++ src/components/containers/app.jsx
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import $ from 'jquery';
 import Banner from './../views/banner/banner.jsx';
 import Nav from './../views/nav/nav.jsx';
 import Breadcrumbs from './../views/breadcrumbs/breadcrumbs.jsx';
-import Menu from './../views/menu/menu.jsx';
-import Video from './../views/video/video.jsx';
+import Content from './../views/content/content.jsx';
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.getVideoData = this.getVideoData.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.state = {};
   }
+
+  getVideoData() {
+    // AXIOS NOT WORKING, USE JQUERY FOR NOW
+    const URL = 'https://www.veritasprep.com/api/desktop-app/get_playlist.php';
+    const body = { type: 'desktop', account: 'GRE' };
+
+    $.post(URL, body)
+    .then(res => {
+      this.setState({ videoData: JSON.parse(res) });
+    })
+    .catch(err => console.log(err));
+  }
+
+  toggle(e) {
+    const that = e.target.style;
+    console.log(that);
+  }
+
+  componentDidMount() {
+    this.getVideoData();
+  }
+
   render() {
     return (
       <div style={ app }>
         <Banner />
         <Nav />
         <Breadcrumbs />
-        <div style={ container }>
-          <Menu />
-          <Video />
-        </div>
+        <Content toggle={ this.toggle } videoData={ this.state.videoData }/>
       </div>
     )
   }
@@ -31,12 +51,5 @@ export default class App extends Component {
 const app = {
   display: 'flex',
   flexDirection: 'column',
+  height: '100vh'
 };
-
-const container = {
-  backgroundColor: 'green',
-  display: 'flex',
-  flexDirection: 'row',
-  width: '100%',
-  height: '800px',
-}
